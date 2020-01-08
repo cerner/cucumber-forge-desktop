17e5d9ba313edf18242ade3cbdcf699a140a9b58
jkuester <jlkstuff@kuester7.com>
2020-01-08 09:12:56 -0600

    Support regenerating the docs when a tag is pushed

---
+++ .travis/deployDocs.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+git checkout ${TRAVIS_BRANCH}
+
+echo Regenerating documentation:
+
+node ./.travis/regenerateDocsReport.js
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git config --global push.default current
+git commit ./docs/index.html -m "chore(docs): Regenerate docs"
+git push https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+
