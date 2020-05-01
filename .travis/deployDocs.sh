258443b946262d84c072c1150a97c84b09c4aeba
Joshua Kuestersteffen <jbkstuff@gmail.com>
2020-05-01 17:04:52 -0500

    chore(doc): Fix issue in the deployDoc.sh script (#42)

--- .travis/deployDocs.sh
+++ .travis/deployDocs.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+git fetch https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master
 git checkout master
 
 echo Regenerating documentation:
@@ -10,4 +11,3 @@ git config --global user.name "Travis CI"
 git config --global push.default current
 git commit ./docs/index.html -m "chore(docs): Regenerate docs"
 git push https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
-
