cc69cb248d535ca257ff80d026dd4685ef9eba47
Tobiah <tobiah@pm.me>
2022-07-22 22:18:13 -0500

    docs: fix docs path

--- .github/workflows/regenerateDocsReport.js
+++ .github/workflows/regenerateDocsReport.js
@@ -5,5 +5,5 @@ const Generator = require('cucumber-forge-report-generator');
 
 const FILE_ENCODING = 'utf-8';
 
-const report = new Generator().generate(path.resolve(__dirname, '../'), 'cucumber-forge-desktop')
-fs.writeFileSync(path.resolve(__dirname, '../docs/index.html'), report, FILE_ENCODING);
+const report = new Generator().generate(path.resolve(__dirname, '../../'), 'cucumber-forge-desktop')
+fs.writeFileSync(path.resolve(__dirname, '../../docs/index.html'), report, FILE_ENCODING);
